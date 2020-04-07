7efc26381289ef7ed5b20fab347b8469bf25d8ea
Marcelo Bini <marcelobini@gmail.com>
2020-04-07 18:59:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+def game_hash
+  teams = {
+    :home => {
+      :team_name "Brooklyn Nets",
+      :colors "Black, White"
+      :players [{player_name: "Alan Anderson",number:0,shoe:16,points:22,rebounds:12,assists:12,steals:3,blocks:1,slam_dunks:1}]
+    }
+    :away =>
+      :team_name => "Flamengo"
+      :colors "Red and Black"
+      :players [{player_name: "",number:,shoe:,points:,rebounds:,assists:,steals:,blocks:,slam_dunks:}]
+  }
+end
