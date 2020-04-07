3e560d0a1d470f64ae89f0a8c916e91e12a025eb
Marcelo Bini <marcelobini@gmail.com>
2020-04-07 19:14:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,6 @@
 def game_hash
   teams = {
-    :home => {
+    :home {
       :team_name "Brooklyn Nets",
       :colors "Black, White",
       :players [{player_name:"Alan Anderson",number:0,shoe:16,points:22,rebounds:12,assists:12,steals:3,blocks:1,slam_dunks:1},
@@ -9,7 +9,7 @@ def game_hash
       {player_name:"Mason Plumlee",number:1,shoe:19,points:26,rebounds:11,assists:6,steals:3,blocks:8,slam_dunks:5},
       {player_name:"Jason Terry",number:31,shoe:15,points:19,rebounds:2,assists:2,steals:4,blocks:11,slam_dunks:1}
     }]
-    :away =>{
+    :away {
       :team_name => "Charlotte Hornets",
       :colors "Turquoise, Purple",
       :players [{player_name:"Jeff Adrien",number:4,shoe:18,points:10,rebounds:1,assists:1,steals:2,blocks:7,slam_dunks:2},
